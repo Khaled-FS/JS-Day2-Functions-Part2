c6b20db4b909ca3295388295808e3fec120467dc
unknown <eng.Khaled10@outlook.com>
2024-08-05 20:44:45 +0300

    hi

---
+++ functions2.js
@@ -0,0 +1,23 @@
+function task(number) {
+  console.log(`-----task ${number}------`);
+}
+
+function isOdd(n) {
+  if (n % 2 == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+task(1);
+let result = isOdd(5);
+console.log(result);
+
+function squareOrDouble(n) {
+  if (n % 2 == 0) {
+    let numE = n;
+  } else {
+    return true;
+  }
+}
