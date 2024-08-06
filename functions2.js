785c34fd70e9ff34bcdbfa6b9f2f8ffac0dd4ec8
unknown <eng.Khaled10@outlook.com>
2024-08-06 17:07:38 +0300

    done

--- functions2.js
+++ functions2.js
@@ -16,8 +16,26 @@ console.log(result);
 
 function squareOrDouble(n) {
   if (n % 2 == 0) {
-    let numE = n;
+    let numE = n * 2;
+    return numE;
   } else {
-    return true;
+    let numE2 = n ** 2;
+    return numE2;
   }
 }
+
+task(3);
+let result2 = squareOrDouble(4);
+console.log(result2);
+
+//---------------------------
+
+function oddsSmallerThan(n) {
+  const num = (n - 1) / 2;
+  if (n % 2 == 1) {
+    return num;
+  }
+}
+task(2);
+let result3 = oddsSmallerThan(5);
+console.log(result3);
